312f5806746da5213555c6c646a1adeb53076fb4
Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2022-10-25 22:23:18 +0300

    Improve map examples (#58)

--- example_test.go
+++ example_test.go
@@ -15,19 +15,19 @@ func ExampleNewTypedMapOf() {
 	type Person struct {
 		GivenName   string
 		FamilyName  string
-		YearOfBirth int
+		YearOfBirth int16
 	}
 	age := xsync.NewTypedMapOf[Person, int](func(seed maphash.Seed, p Person) uint64 {
 		var h maphash.Hash
 		h.SetSeed(seed)
-		// We write field numbers, to make sure that "foo", "bar" and "foob", "ar" hash differently.
-		h.WriteByte(0)
 		h.WriteString(p.GivenName)
-		h.WriteByte(1)
+		hash := h.Sum64()
+		h.Reset()
 		h.WriteString(p.FamilyName)
-		h.WriteByte(2)
+		hash = 31*hash + h.Sum64()
+		h.Reset()
 		binary.Write(&h, binary.LittleEndian, p.YearOfBirth)
-		return h.Sum64()
+		return 31*hash + h.Sum64()
 	})
 	Y := time.Now().Year()
 	age.Store(Person{"Ada", "Lovelace", 1815}, Y-1815)
