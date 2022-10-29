dbd80c5572b3ecb11c17f0c16697dd28a848d52e
Chris Duncan <veqryn@hotmail.com>
2022-10-29 05:52:42 -0600

    Add Compute method draft to MapOf (#68)

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ package xsync_test
 
 import (
 	"encoding/binary"
+	"fmt"
 	"hash/maphash"
 	"time"
 
@@ -32,4 +33,54 @@ func ExampleNewTypedMapOf() {
 	Y := time.Now().Year()
 	age.Store(Person{"Ada", "Lovelace", 1815}, Y-1815)
 	age.Store(Person{"Charles", "Babbage", 1791}, Y-1791)
+}
+
+func ExampleMapOf_Compute() {
+	counts := xsync.NewIntegerMapOf[int, int]()
+
+	// Store a new value.
+	v, ok := counts.Compute(42, func(oldValue int, loaded bool) (newValue int, delete bool) {
+		// loaded is false here.
+		newValue = 42
+		delete = false
+		return
+	})
+	// v: 42, ok: true
+	fmt.Printf("v: %v, ok: %v\n", v, ok)
+
+	// Update an existing value.
+	v, ok = counts.Compute(42, func(oldValue int, loaded bool) (newValue int, delete bool) {
+		// loaded is true here.
+		newValue = oldValue + 42
+		delete = false
+		return
+	})
+	// v: 84, ok: true
+	fmt.Printf("v: %v, ok: %v\n", v, ok)
+
+	// Compare existing value, and set new value or keep old value, and store the oldVal
+	var oldVal int
+	minVal := 63
+	v, ok = counts.Compute(42, func(oldValue int, loaded bool) (newValue int, delete bool) {
+		oldVal = oldValue
+		if !loaded || oldValue < minVal {
+			newValue = minVal
+			delete = false
+			return
+		}
+		newValue = oldValue
+		delete = false
+		return
+	})
+	// v: 84, ok: true, oldVal: 84
+	fmt.Printf("v: %v, ok: %v, oldVal: %v\n", v, ok, oldVal)
+
+	// Delete an existing value.
+	v, ok = counts.Compute(42, func(oldValue int, loaded bool) (newValue int, delete bool) {
+		// loaded is true here.
+		delete = true
+		return
+	})
+	// v: 84, ok: false
+	fmt.Printf("v: %v, ok: %v\n", v, ok)
 }
