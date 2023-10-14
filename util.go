3a0ff0d301b1c63c0bf911eb1ea593010e578fad
destel <destel.od@gmail.com>
2023-10-14 17:38:07 +0300

    Generic hash function

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 package xsync
 
 import (
+	"encoding/binary"
+	"fmt"
 	"hash/maphash"
 	"reflect"
 	"runtime"
@@ -52,6 +54,61 @@ func hashString(seed maphash.Seed, s string) uint64 {
 	}
 	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	return uint64(memhash(unsafe.Pointer(strh.Data), uintptr(seed64), uintptr(strh.Len)))
+}
+
+func MakeHashFunc[T comparable]() func(T) uint64 {
+	var zero T
+	rt := reflect.TypeOf(&zero).Elem() // Elem() avoids panic when T is interface
+
+	seed := maphash.MakeSeed()
+	seed64 := *(*uint64)(unsafe.Pointer(&seed))
+
+	switch rt.Kind() {
+	// various integers and pointers
+	case reflect.Int, reflect.Uint, reflect.Int8, reflect.Uint8, reflect.Int16, reflect.Uint16, reflect.Int32, reflect.Uint32, reflect.Int64, reflect.Uint64,
+		reflect.Pointer, reflect.UnsafePointer, reflect.Chan:
+		return func(v T) uint64 {
+
+			return uint64(*(*uintptr)(unsafe.Pointer(&v)))
+		}
+
+	// strings use the same trick as in hashString()
+	case reflect.String:
+		return func(v T) uint64 {
+			strh := (*reflect.StringHeader)(unsafe.Pointer(&v))
+			return uint64(memhash(unsafe.Pointer(strh.Data), uintptr(seed64), uintptr(strh.Len)))
+		}
+
+	// other comparable fixed-size types
+	case reflect.Struct, reflect.Array, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
+		seed := maphash.MakeSeed()
+		seed64 := *(*uint64)(unsafe.Pointer(&seed))
+		return func(v T) uint64 {
+			valSize := unsafe.Sizeof(v)
+			return uint64(memhash(unsafe.Pointer(&v), uintptr(seed64), valSize))
+		}
+
+	// Implementation for interfaces is slow and can throw a panic at runtime.
+	// Technically we should never get here because this is a generic function used for
+	// generic hash-maps.
+	// Maybe it better to disable this case at all?
+	case reflect.Interface:
+		return func(t T) uint64 {
+			var h maphash.Hash
+			h.SetSeed(seed)
+			err := binary.Write(&h, binary.LittleEndian, t)
+			if err != nil {
+				panic(err)
+			}
+			return h.Sum64()
+		}
+
+	// This should never happen as well, especially taking into account
+	// that T is comparable.
+	default:
+		panic(fmt.Sprintf("unsupported type %v", rt))
+
+	}
 }
 
 //go:noescape
--- util_test.go
+++ util_test.go
@@ -1,9 +1,11 @@
 package xsync_test
 
 import (
+	"fmt"
 	"hash/maphash"
 	"math/rand"
 	"testing"
+	"unsafe"
 
 	. "github.com/puzpuzpuz/xsync/v2"
 )
@@ -21,6 +23,66 @@ func TestNextPowOf2(t *testing.T) {
 	if NextPowOf2(3) != 4 {
 		t.Error("nextPowOf2 failed")
 	}
+}
+
+func doTestMakeHashFunc[T comparable](t *testing.T, val1, val2 T) {
+	t.Helper()
+
+	if val1 == val2 {
+		t.Error("use two different values for the test")
+	}
+
+	hash := MakeHashFunc[T]()
+
+	val1copy := val1
+	if hash(val1) != hash(val1) || hash(val1copy) != hash(val1copy) {
+		t.Error("two invocations of hash for the same value return different results")
+	}
+
+	// double check
+	val2copy := val2
+	if hash(val2) != hash(val2) || hash(val2copy) != hash(val2copy) {
+		t.Error("two invocations of hash for the same value return different results")
+	}
+
+	// Test that different values have different hashes.
+	// That's not always the case, so we'll try multiple hash functions,
+	// to make probability of failure veirtually zero
+	for i := 0; ; i++ {
+		if hash(val1) != hash(val2) {
+			break
+		}
+
+		if i >= 20 {
+			t.Error("Different values have the same hash")
+			break
+		}
+
+		t.Log("Different values have the same hash, trying another hash function")
+		hash = MakeHashFunc[T]()
+	}
+}
+
+func TestMakeHashFunc(t *testing.T) {
+	type Point struct {
+		X, Y int
+	}
+
+	doTestMakeHashFunc(t, int32(116), int32(117))
+	doTestMakeHashFunc(t, 3.1415, 2.7182)
+	doTestMakeHashFunc(t, "foo", "bar")
+	doTestMakeHashFunc(t, Point{1, 2}, Point{3, 4})
+	doTestMakeHashFunc(t, [3]byte{'f', 'o', 'o'}, [3]byte{'b', 'a', 'r'})
+
+	doTestMakeHashFunc(t, &Point{1, 2}, &Point{1, 2})
+	doTestMakeHashFunc(t, nil, &Point{1, 2})
+	doTestMakeHashFunc(t, unsafe.Pointer(&Point{1, 2}), unsafe.Pointer(&Point{1, 2}))
+	doTestMakeHashFunc(t, make(chan string), make(chan string))
+
+	// works only in go 1.20+
+	//var a any = Point{1, 2}
+	//var b any = Point{3, 4}
+	//doTestMakeHashFunc(t, a, b)
 }
 
 // This test is here to catch potential problems
@@ -73,4 +135,40 @@ func BenchmarkHashString(b *testing.B) {
 		_ = HashString(seed, benchmarkKeyPrefix)
 	}
 	// about 4ns/op on x86-64
+}
+
+func doBenchmarkMakeHashFunc[T comparable](b *testing.B, val T) {
+	hash := MakeHashFunc[T]()
+	b.Run(fmt.Sprintf("%T hash", val), func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_ = hash(val)
+		}
+	})
+
+	// Since hashing is a function call, let's wrap map read into a function call as well.
+	// Seems this should give better comparison of the performance of the hash function itself.
+	m := map[T]int{val: 10}
+	readMap := func() int {
+		return m[val]
+	}
+
+	b.Run(fmt.Sprintf("%T map read", val), func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_ = readMap()
+		}
+	})
+}
+
+func BenchmarkMakeHashFunc(b *testing.B) {
+	type Point struct {
+		X, Y, Z int
+	}
+
+	doBenchmarkMakeHashFunc(b, 116)
+	doBenchmarkMakeHashFunc(b, "test key")
+	doBenchmarkMakeHashFunc(b, Point{1, 2, 3})
+	doBenchmarkMakeHashFunc(b, &Point{1, 2, 3})
+
 }
