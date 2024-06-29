45aa7d99ee17c95483158120cc27321947f5a732
Andrey Pechkurov <apechkurov@gmail.com>
2024-06-29 21:29:24 +0300

    wip

--- export_test.go
+++ export_test.go
@@ -45,6 +45,26 @@ func Fastrand() uint32 {
 	return runtime_fastrand()
 }
 
+func Broadcast(b uint8) uint64 {
+	return broadcast(b)
+}
+
+func IndexOfFirstMarkedByte(w uint64) int {
+	return indexOfFirstMarkedByte(w)
+}
+
+func MarkZeroBytes(w uint64) uint64 {
+	return markZeroBytes(w)
+}
+
+func ZeroByte(w uint64, idx int) uint64 {
+	return zeroByte(w, idx)
+}
+
+func SetByte(w uint64, b uint8, idx int) uint64 {
+	return setByte(w, b, idx)
+}
+
 func NextPowOf2(v uint32) uint32 {
 	return nextPowOf2(v)
 }
